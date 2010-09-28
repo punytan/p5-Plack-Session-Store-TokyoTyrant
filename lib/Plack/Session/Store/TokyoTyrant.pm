37d29933ae5c54920c0952b6db9225bd88b0672b
punytan <punytan@gmail.com>
2010-09-28 10:14:49 +0900

    implemented prototype

--- lib/Plack/Session/Store/TokyoTyrant.pm
+++ lib/Plack/Session/Store/TokyoTyrant.pm
@@ -1,7 +1,71 @@
 package Plack::Session::Store::TokyoTyrant;
 use strict;
 use warnings;
+
 our $VERSION = '0.01';
+
+use Storable ();
+use MIME::Base64 ();
+
+use TokyoTyrant ();
+
+use parent 'Plack::Session::Store';
+
+use Plack::Util::Accessor qw[prefix rdb serializer deserializer];
+
+sub new {
+    my ($class, %args) = @_;
+
+    unless (ref $args{server} eq 'ARRAY') {
+        die "'server' argument must be an arrayref";
+    }
+
+    $args{key_prefix} ||= 'sessions';
+    $args{separator}  ||= ':';
+
+    # stolen from Plack::Session::Store::DBI ;)
+    $args{serializer} ||=
+        sub { MIME::Base64::encode_base64( Storable::nfreeze($_[0]) ) };
+    $args{deserializer} ||=
+        sub { Storable::thaw( MIME::Base64::decode_base64($_[0]) ) };
+
+    my $rdb = TokyoTyrant::RDB->new;
+    $rdb->open( @{$args{server}} ) or die $rdb->errmsg($rdb->ecode);
+
+    my %params = (
+        rdb          => $rdb,
+        prefix       => $args{key_prefix} . $args{separator},
+        serializer   => $args{serializer},
+        deserializer => $args{deserializer},
+    );
+
+    return bless \%params, $class;
+}
+
+sub fetch {
+    my ($self, $session_id) = @_;
+    my $key = $self->prefix . $session_id;
+
+    my $value = $self->rdb->get($key);
+    return defined $value ? $self->deserializer->($value) : undef;
+}
+
+sub store {
+    my ($self, $session_id, $session) = @_;
+    my $key = $self->prefix . $session_id;
+
+    $self->rdb->put($key, $self->serializer->($session))
+        or warn $self->rdb->errmsg($self->rdb->ecode);
+
+}
+
+sub remove {
+    my ($self, $session_id) = @_;
+    my $key = $self->prefix . $session_id;
+
+    $self->rdb->out($key)
+        or warn $self->rdb->errmsg($self->rdb->ecode);
+}
 
 1;
 __END__
